b2d9e9f834302463aa4dc60199c06ef8988be0d6
Mohit Kumar <39983059+anodit@users.noreply.github.com>
2019-10-12 11:04:08 +0530

    Add files via upload

---
+++ Miscellaneous_Programs/array_sum.py
@@ -0,0 +1,8 @@
+# Sum of elements in a given array
+def sum_array(arr, n):
+    return sum(arr)  #Inbuild Function sum() 
+arr = []
+arr = [2, 4, 6, 8, 10]
+n = len(arr)
+ans = sum_array(arr, n)
+print('Sum of array is : ', ans)
